7298b5b7ab87d180877496d641dd968f7e4af535
Ahmet Karakaya <karakayahmet00@gmail.com>
2023-02-28 12:38:17 +0300

    entity sınıflarının oluşturulması tamamlandı.

--- bolum1/Program.cs
+++ bolum1/Program.cs
@@ -1,8 +1,27 @@
-﻿namespace bolum1;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bolum1;
+
+public class Product{
+
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    [Required]
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+}
+
+public class Category{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
 }
